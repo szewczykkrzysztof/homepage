76d63dc29d608952459e1d12e2ee17b233f861c5
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-05-27 10:31:38 +0200

    added function, that inject new article to page

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const firstName = "Krzysiek";
 const age = 42;
 console.log(`Mam na imię ${firstName} i mam ${age} lata`);
+const aboutMe = document.querySelector(".notes-article--js");
+console.log(aboutMe);
+aboutMe.innerHTML = `<h3>Witam tu ${firstName}</h3> To jest artykuł wygenerowany poprzez wstrzyknięcie
+ tego tekstu do kodu DOM za pomocą funkcji innerHTML`;
+console.log(aboutMe);
