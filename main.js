6274a0ff538a2bc2f4287deee5a5db4564984ffe
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-06-03 12:36:54 +0200

    created function that welcome myself and inject html code to page

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ console.log(aboutMe);
 aboutMe.innerHTML = `<h3>Witam tu ${firstName}</h3> To jest artykuł wygenerowany poprzez wstrzyknięcie
  tego tekstu do kodu DOM za pomocą funkcji innerHTML`;
 console.log(aboutMe);
+
+function myName(name,age) {
+    return (`Nazywam się ${name} i mam ${age} lat`);
+};
+
+function calculate(number) {
+    square = number * number;
+    return `Kwadrat liczby ${number} to ${square}`;
+};
+
+console.log(calculate(5));
+
+console.log(myName(firstName,age));
+
+const aboutMeFunction = document.querySelector(".notes-article--welcomeFunction");
+
+aboutMeFunction.innerHTML = myName('Tymon',48);
